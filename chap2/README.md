3efb1dea1442b7ed3fb38d4614d5f0e79aaf4504
Baochuan Lu <blu@sbuniv.edu>
2018-02-01 15:06:28 +0000

    add README.md

---
+++ chap2/README.md
@@ -0,0 +1,16 @@
+# Code from CSAPP Chapter 2
+
+## Compile
+Compile show_bytes.c into a 32bit binary and double check.
+```
+gcc -o show_bytes -m32 show_bytes.c
+file show_bytes
+```
+
+## Experiment
+Edit and run show_bytes to study the bit representations of various data types.
+```
+./show_bytes
+./show_bytes 1
+./show_bytes -1
+```
--- chap2/show_bytes.c
+++ chap2/show_bytes.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef unsigned char *byte_pointer;
 
 void show_bytes(byte_pointer start, int len) {
@@ -30,11 +33,76 @@ void test_show_bytes(int val) {
   show_pointer(pval);
 }
 
-int bitAnd(int x, int y) {
-  return 2;
+void simple_show_a() {
+  int val = 0x87654321;
+  byte_pointer valp = (byte_pointer) &val;
+  show_bytes(valp, 1); /* A. */
+  show_bytes(valp, 2); /* B. */
+  show_bytes(valp, 3); /* C. */
 }
 
-int main(void) {
-  test_show_bytes(4);
+void simple_show_b() {
+  int val = 0x12345678;
+  byte_pointer valp = (byte_pointer) &val;
+  show_bytes(valp, 1); /* A. */
+  show_bytes(valp, 2); /* B. */
+  show_bytes(valp, 3); /* C. */
+}
+
+void float_eg() {
+  int x = 3490593;
+  float f = (float) x;
+  printf("For x = %d\n", x);
+  show_int(x);
+  show_float(f);
+
+  x = 3510593;
+  f = (float) x;
+  printf("For x = %d\n", x);
+  show_int(x);
+  show_float(f);
+}
+
+void string_ueg() {
+  const char *s = "ABCDEF";
+  show_bytes((byte_pointer) s, strlen(s));
+}
+
+void string_leg() {
+  const char *s = "abcdef";
+  show_bytes((byte_pointer) s, strlen(s));
+}
+
+void show_twocomp() {
+  short x = 12345;
+  short mx = -x;
+
+  show_bytes((byte_pointer) &x, sizeof(short));
+  show_bytes((byte_pointer) &mx, sizeof(short));
+}
+
+int main(int argc, char *argv[]) {
+  int val = 12345;
+
+  if (argc > 1) {
+    if (argc > 1) {
+      val = strtol(argv[1], NULL, 0);
+    }
+    printf("calling test_show_bytes\n");
+    test_show_bytes(val);
+  } else {
+    printf("calling show_twocomp\n");
+    show_twocomp();
+    printf("Calling simple_show_a\n");
+    simple_show_a();
+    printf("Calling simple_show_b\n");
+    simple_show_b();
+    printf("Calling float_eg\n");
+    float_eg();
+    printf("Calling string_ueg\n");
+    string_ueg();
+    printf("Calling string_leg\n");
+    string_leg();
+  }
   return 0;
 }
